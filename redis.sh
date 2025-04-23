3bc04d79073d15fbffcea9a7a38a77105b7e55fd
krishna <krishna.vennapusa@gmail.com>
2025-04-24 03:00:17 +0530

    99% practice

---
+++ redis.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+ID=$(id -u)
+Timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+logfile=/tmp/$0-$Timestamp
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE( ) {
+    if [ $? -ne 0 ]; then
+
+        echo -e " $R ERROR...!$2........ is FAILED $N"
+        exit 1
+    else
+
+        echo -e " $G SUCCESS..! $2............ is SUCCESS $N"
+
+    fi
+}
+
+if [ $ID -ne 0 ]; then
+
+    echo -e " $R ERROR....! YOU are NOT a ROOT USER $N "
+    exit 1
+
+else
+
+    echo -e " $G SUCCESS...! YOU are ROOT USER..PLEASE PROCCED $N "
+
+fi
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $logfile
+
+VALIDATE $? "DOWNLOAD redis rpm file"
+
+dnf module enable redis:remi-6.2 -y &>> $logfile
+
+VALIDATE $? "enabeling redis rpm file"
+
+dnf install redis -y &>> $logfile
+
+VALIDATE $? "installing redis rpm file"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $logfile
+
+VALIDATE $? "modifying the IP address"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $logfile
+
+VALIDATE $? "Modifying the redis ip address in conf file"
+
+systemctl enable redis &>> $logfile
+
+VALIDATE $? "enabeling redis service"
+
+systemctl start redis &>> $logfile
+
+VALIDATE $? "starting the redis service"
