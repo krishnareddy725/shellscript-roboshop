19ccbf8b4a3bc4e93f3e1148654b8b192ead6ff3
krishna <krishna.vennapusa@gmail.com>
2025-04-23 14:38:38 +0530

    99% Practice

---
+++ mongodb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+logfile=/tmp/$0-TIMESTAMP
+echo "Script started executing at $TIMESTAMP" &>> $logfile
+
+VALIDATE( ) {
+    if [ $? -ne 0]; then
+        echo -e "$2 .............$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 .............$G Success $N"
+}
