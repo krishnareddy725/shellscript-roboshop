acb0f1182932566aac36dff5ba4c71cbc40c585f
krishna <krishna.vennapusa@gmail.com>
2025-04-24 18:22:26 +0530

    99% practice

---
+++ realissues/oldfiles2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source_directory="/tmp/"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $source_directory ]; then
+
+    echo -e "$R source directory is not found $source_directory"
+    
+fi
+
+files_to_delete=$(find $source_directory -type f -mtime +15 -name "*.log")
+
+while IFS= read -r line; do
+    echo -e "$R deleting file is: $line"
+    rm -rf $line
+done <<< "$files_to_delete"
