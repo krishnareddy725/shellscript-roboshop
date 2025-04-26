3db3e539d6699b35f84ac4876eb607e0f2c21a55
krishna <krishna.vennapusa@gmail.com>
2025-04-26 20:25:15 +0530

    99% practice

--- realissues/oldlogsfiles.sh
+++ realissues/oldlogsfiles.sh
@@ -9,10 +9,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-files_to_delete=$(find source_directry -type f -mtime +14 -name "*.log")
+if [ ! -d $source_directory ]; then
+    echo -e " $R source directory is not avilable : $source_directory"
+fi
+
+files_to_delete=$(find $source_directry -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line; do
     echo -e " delete file: $line"
     rm -rf $line
-done <<< files_to_delete
+done <<< "files_to_delete"
 
